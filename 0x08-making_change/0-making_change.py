fd68640af44b105e8098d27eb9fdc36c067e026a
Blessing <ogorblessing96@gmail.com>
2024-11-28 16:00:22 +0100

    Made changes

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,22 @@
+#!usr?bin/python3
+"""Defines the function"""
+
+def makeChange(coins, total):
+    """Returns the fewest number of coins needed to meet total
+    """
+    if total <= 0:
+        return 0
+    if len(coins) is 0:
+        return -1
+    coins = sorted(coins)
+    dynamic = [float('inf')] * (total + 1)
+    dynamic[0] = 0
+    for i in range(total + 1):
+        for coin in coins:
+            if coin > i:
+                break
+            if dynamic[i - coin] != -1:
+                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
+    if dynamic[total] == float('inf'):
+        return -1
+    return dynamic[total]
