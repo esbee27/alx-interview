0dfe586ef77da75409c13e081353a6aa5ecc32c3
Blessing <ogorblessing96@gmail.com>
2024-12-11 13:16:13 +0100

    Created a file that handled the prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -5,24 +5,24 @@ def isWinner(x, nums):
     """Function that performs prime game"""
     if not nums or x < 1:
         return None
-        n = max(nums)
-        filt = [True for _ in range(max(n + 1, 2))]
-        for i in range(2, int(pow(n, 0.5)) + 1):
-            if not filt[i]:
-                continue
-            for j in range(i * i, n + 1, i):
-                filt[j] = False
-        filt[0] = filt[1] = False
-        d = 0
-        for i in range(len(filt)):
-            if filt[i]:
-                d += 1
-            filt[i] = d
-        plyr1 = 0
-        for n in nums:
-            plyr1 += filt[n] % 2 == 1
-        if plyr1 * 2 == len(nums):
-            return None
-        if plyr1 * 2 > len(nums):
-            return "Maria"
-        return "Ben"
+    n = max(nums)
+    filt = [True for _ in range(max(n + 1, 2))]
+    for i in range(2, int(pow(n, 0.5)) + 1):
+        if not filt[i]:
+            continue
+        for j in range(i * i, n + 1, i):
+            filt[j] = False
+    filt[0] = filt[1] = False
+    d = 0
+    for i in range(len(filt)):
+        if filt[i]:
+            d += 1
+        filt[i] = d
+    plyr1 = 0
+    for n in nums:
+        plyr1 += filt[n] % 2 == 1
+    if plyr1 * 2 == len(nums):
+        return None
+    if plyr1 * 2 > len(nums):
+        return "Maria"
+    return "Ben"
